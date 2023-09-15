40883d418cc464aee0b860c81431f4ab46ab12f8
Cyber-G <cygerpham@free.fr>
2023-09-15 19:33:52 +0200

    fractional_time_delay_refactor.py: rewrite some parameters with more meaning to me

---
+++ fractional_time_delay_refactor.py
@@ -0,0 +1,47 @@
+# https://dsp.stackexchange.com/questions/60476/how-to-do-fft-fractional-time-delay-solved
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+f1 = 12.8
+f2 = 22.6
+samples = 2**13
+fs = 10000.
+tDelay = 0.1234/f1
+tstart = 0.0
+tend = samples/fs
+
+# 0. Example waveform to demonstrate the time shift
+timeList = np.arange(tstart, tend, 1/fs)
+waveform = np.sin(2 * np.pi * f1 * timeList) + 1*np.sin(2 * np.pi * f2 * timeList)
+
+# 1. Take the FFT
+fftData = np.fft.fft(waveform)
+
+# 2. Construct the phase shift
+samplePeriod = 1/fs
+tDelayInSamples = tDelay / samplePeriod
+N = fftData.shape[0]
+k = np.linspace(0, N-1, N)
+timeDelayPhaseShift = np.exp(((-2*np.pi*1j*k*tDelayInSamples)/(N)) + (tDelayInSamples*np.pi*1j))
+
+# 3. Do the fftshift on the phase shift coefficients
+timeDelayPhaseShift = np.fft.fftshift(timeDelayPhaseShift)
+
+# 4. Multiply the fft data with the coefficients to apply the time shift
+fftWithDelay = np.multiply(fftData, timeDelayPhaseShift)
+
+# 5. Do the IFFT
+shiftedWaveform = np.fft.ifft(fftWithDelay)
+
+print("\nThe sampling period is %f seconds" % samplePeriod)
+print("The time delay is %f seconds" % tDelay)
+print("The time delay in samples is %f samples" % tDelayInSamples)
+print("The correction phase shift is %f pi" % (tDelayInSamples))
+
+plt.figure()
+plots = 1
+plt.subplot(plots, 1, 1)
+plt.plot(waveform)
+plt.plot(shiftedWaveform)
+plt.show()
