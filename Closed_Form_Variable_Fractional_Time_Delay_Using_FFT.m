a2556dea76fa8028df50aa927f68ec8d7817fe7e
Cyber-G <cygerpham@free.fr>
2023-09-19 23:34:28 +0200

    Closed_Form_Variable_Fractional_Time_Delay_Using_FFT.m: implement paper

---
+++ Closed_Form_Variable_Fractional_Time_Delay_Using_FFT.m
@@ -0,0 +1,41 @@
+% A script that implements 
+% S. -C. Pei and Y. -C. Lai, "Closed Form Variable Fractional Time Delay Using FFT," in IEEE Signal Processing Letters, vol. 19, no. 5, pp. 299-302, May 2012, doi: 10.1109/LSP.2012.2191280.
+% URL: https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=6171834&isnumber=6167335
+
+% Author: Germain Pham
+% Date: Sept. 2023
+
+phi             = @(t,p)    1/sqrt(2^p*factorial(p)*sqrt(pi))*exp(-t.^2/2).*hermiteH(p,t);
+test_function   = @(t)      phi(t,5) + phi(t,12);
+
+
+N   = 64;
+t   = linspace(-7,7,N);
+Ts  = diff(t(1:2));
+y_0 = test_function(t);
+
+% plot(t,y_0,'s-')
+
+
+delay_vec = 0:0.1:10;
+% for each delay, compute equation 4
+for i = 1:length(delay_vec)
+    delay       = delay_vec(i);
+    H           = [ 1 ... 
+                    exp(-1i*2*pi*delay/N*(1:((N/2)-1))) ...
+                    cos(delay*pi) ...
+                    exp(1i*2*pi*delay/N*(N-(((N/2)+1):(N-1))))]; % equation 4
+    H_simpler   = ifftshift(exp(-1i*2*pi*delay/N*(-N/2:(N/2-1))));
+    H_simpler(N/2+1) = H_simpler(N/2+1) - 1i*sin(delay*pi); % compensate Tarczynski error (Cain and Yardim, 1994)
+    % H == H_simpler
+    y_delayed_fft_based = ifft(fft(y_0).*H); % equation 5
+    y_delayed_truth     = test_function(t-delay*Ts);
+    RMSD(i) = sqrt(mean(abs(y_delayed_fft_based-y_delayed_truth).^2));
+end
+
+figure
+plot(delay_vec,RMSD)
+
+figure
+plot([y_delayed_fft_based(:) y_delayed_truth(:)])
+legend('fft-based','truth')
